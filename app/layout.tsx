35415a4d82a1c65cc715f86307411869cb1ad48b
Damian Marrone <damianmarrone@MacBook-Pro-de-Damian.local>
2025-08-08 20:06:22 -0300

    fix: restore tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,38 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en">
             <head>
-               
+                {/* Google Tag Manager */}
+                <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MXD9LTVF');
+            `,
+                    }}
+                />
+
+                {/* Google Analytics (si no lo tenés dentro de GTM) */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-FZHM4V16ZF"
+                    strategy="afterInteractive"
+                />
+                <Script
+                    id="ga-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FZHM4V16ZF');
+            `,
+                    }}
+                />
             </head>
             <body>
                 {/* GTM fallback para usuarios sin JS */}
